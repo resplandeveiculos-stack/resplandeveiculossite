145efcef8f3776e59474a75708837defd877ffa1
null <null>
2026-04-06 19:23:35 -0300

    newcodebase

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Esses dados são exclusivos do seu projeto "Resplande Veículos"
-const supabaseUrl = 'https://usiolknyxcpvnslvbrzv.supabase.co';
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzaW9sa255eGNwdm5zbHZicnp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyMTUzMDIsImV4cCI6MjA4OTc5MTMwMn0.0t5edQuAittli91GtQZm7SEAyKfICiTvrGWZPpHe0RA';
+// Sua nova URL e nova Chave que você acabou de criar!
+const supabaseUrl = 'https://tsgqfolyfnziyuekxwyj.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzZ3Fmb2x5Zm56aXl1ZWt4d3lqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1MTA3MTUsImV4cCI6MjA5MTA4NjcxNX0.79RPieffV1TbCJEOrfvUixVHRg79FCeiDEAeQaBtMS0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
